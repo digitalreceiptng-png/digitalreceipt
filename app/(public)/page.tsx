ed910d63e10bf81280e1a0edb007e174f11cbaea
Ayodele Victor Olaiya <ayvicola@gmail.com>
2026-06-24 23:14:52 +0100

    Fix layout flash: use CSS media classes instead of JS useIsMobile hook

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,10 +1,15 @@
-'use client'
-
-import { useIsMobile } from '@/hooks/useIsMobile'
 import MobileHomePage from '@/components/mobile/MobileHomePage'
 import DesktopHomePage from '@/components/desktop/DesktopHomePage'
 
 export default function LandingPage() {
-  const isMobile = useIsMobile()
-  return isMobile ? <MobileHomePage /> : <DesktopHomePage />
+  return (
+    <>
+      <div className="md:hidden">
+        <MobileHomePage />
+      </div>
+      <div className="hidden md:block">
+        <DesktopHomePage />
+      </div>
+    </>
+  )
 }
